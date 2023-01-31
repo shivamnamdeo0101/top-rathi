cbb5737b894b412ae82b0489fbcd36ba06058620
shinamde <Wsxokn@#14>
2023-01-31 16:32:17 +0530

    sch-filter

--- src/utils/EndPoint.js
+++ src/utils/EndPoint.js
@@ -1,2 +1,2 @@
-export const EndPoint = 'http://localhost:5000/api/';
-// export const EndPoint = 'https://toprathi-admin.onrender.com/api/';
+// export const EndPoint = 'http://localhost:5000/api/';
+export const EndPoint = 'https://toprathi-admin.onrender.com/api/';
